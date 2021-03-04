4db9781226a099b22ad11575c8383a5339467512
stiven.lopez <Stiven.lopez2>
2021-03-04 08:38:12 -0500

    [UPDATE] scripts de transformacion y estimador

--- src/sklearn_preprocessor.py
+++ src/sklearn_preprocessor.py
@@ -1,14 +1,11 @@
 from __future__ import print_function
 import os
-import json
 import joblib
 import argparse
 import pandas as pd
-from io import StringIO
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import RobustScaler
 from sklearn.pipeline import Pipeline, FeatureUnion
-from sklearn.ensemble import GradientBoostingRegressor
 from pipeline.custom_pipeline import ColumnsSelector, GetDummies, GetDataFrame
 from config.config import numerical_features, categorical_features, target, features
 
@@ -39,13 +36,9 @@ if __name__ == '__main__':
             ('numeric', numeric_preprocessing),
             ('categoric', categorical_preprocessing)]))])
 
-    model = Pipeline(steps=[('preprocessing', preprocessing),
-                            ('gradient_boosting', GradientBoostingRegressor(loss='huber', learning_rate=0.08,
-                                                                            n_estimators=5, subsample=0.7))])
-
-    model.fit(data, target)
-    joblib.dump(model, filename=os.path.join(args.model_dir, 'model.joblib'))
-    print('The model has been saved!')
+    preprocessing.fit(data)
+    joblib.dump(preprocessing, filename=os.path.join(args.model_dir, 'preprocessor.joblib'))
+    print('The preprocessor has been saved!')
 
 
 def model_fn(model_dir):
@@ -55,40 +48,3 @@ def model_fn(model_dir):
     """
     model = joblib.load(os.path.join(model_dir, "preprocessor.joblib"))
     return model
-
-
-def input_fn(input_data, content_type):
-    """
-    Toma los datos de entrada y los carga
-    """
-    if content_type == 'text/csv':
-        df = pd.read_csv(StringIO(input_data), sep=',', header=None)
-        if len(df.columns) == len(features) + 1:
-            df.columns = features + [target]
-        elif len(df.columns) == len(features):
-            df.columns = features
-        return df
-    else:
-        raise ValueError(f'{content_type} not supported by script')
-
-
-def predict_fn(input_data, model):
-    features = model.transform(input_data)
-    if target in input_data:
-        return np.insert(features, 0, input_data[target], axis=1)
-    else:
-        return features
-
-
-def output_fn(prediction, accept):
-    if accept == 'application/json':
-        instances = []
-        for row in prediction.tolist():
-            instances.append({'features': row})
-
-        json_output = {'instances': instances}
-        return worker.Response(json.dumps(json_output), accept=accept, mimetype=accept)
-    elif accept == 'text/csv':
-        return worker.Response(encoders.encode(prediction, accept), accept=accept, mimetype=accept)
-    else:
-        raise RuntimeError(f'{accept} accept type is not supported by this script.')
